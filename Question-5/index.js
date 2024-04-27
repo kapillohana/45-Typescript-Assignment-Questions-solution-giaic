e13fcf3caf8edbbc39e126208a8b67f8ce2a3fb3
kapillohana <kapillohana79@gmail.com>
2024-04-27 16:03:28 +0500

    Done with 5th task of 45

---
+++ Question-5/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = ("Albert Einstein");
+console.log(`"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”
+${famous_person}`);
---
+++ Question-5/index.ts
@@ -0,0 +1,3 @@
+let famous_person=("Albert Einstein");
+ console.log(`"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”
+${famous_person}`)
