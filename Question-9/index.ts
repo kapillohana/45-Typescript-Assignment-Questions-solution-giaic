ef1a9f9705de08e8cbfb64b598d6a9dbf6835474
kapillohana <kapillohana79@gmail.com>
2024-05-12 23:27:15 +0500

    Done with Question-9

---
+++ Question-9/index.ts
@@ -0,0 +1,5 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favNumber:number = 21
+let message: string = `my favourite number is ${favNumber}`;
+console.log (message);
