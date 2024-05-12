90e9e05310f3db3c099d42ccc381eeeb561cba29
kapillohana <kapillohana79@gmail.com>
2024-05-12 23:41:03 +0500

    Done with Question-10

---
+++ Question -10/index.js
@@ -0,0 +1,13 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+//If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. 
+//Then write one sentence describing what the program does.
+//program written by ("kapil")
+// date 12-05-2024
+//this program calculates the area of rectangle
+var Length = 5; // length of the rectangle
+var width = 3; // width of the rectangle
+//calculates the area of the rectangle
+var area = Length * width;
+// print the area of rectange
+//console.log `The area of rectangle is ${area}`
+console.log("the area of rectangle is ", area);
---
+++ Question -10/index.ts
@@ -0,0 +1,17 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+//If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. 
+//Then write one sentence describing what the program does.
+
+//program written by ("kapil")
+// date 12-05-2024
+
+//this program calculates the area of rectangle
+
+let Length :number= 5 // length of the rectangle
+let width: number = 3 // width of the rectangle
+
+//calculates the area of the rectangle
+let area: number = Length* width;
+
+// print the area of rectange
+console.log ("the area of rectangle is ", area)
