bfa696d0babb2b65ccd429a4e67edfa424121ed7
kapillohana <kapillohana79@gmail.com>
2024-05-13 18:35:31 +0500

    Question-12

---
+++ Question-13/index.ts
@@ -0,0 +1,18 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car,
+// and make a list that stores several examples. Use your list to print a series of statements about these items, 
+//such as “I would like to own a Honda motorcycle.”
+
+let favtransportation = ["sports bike", "Honda Civic", "Mercedes", "Sportscar"]
+
+console.log ("with the help of forEach function")
+
+favtransportation.forEach(favtransportation => {
+    console.log(`I would like to own a ${favtransportation}.`);
+});
+
+
+console.log ("with the help of loops")
+
+for (let i=0; i<favtransportation.length; i++){
+    console.log (`I would like to own a ${favtransportation[i]}`)
+}
