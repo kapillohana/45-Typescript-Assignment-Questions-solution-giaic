3e2fa2e3beeb8413e5275539499afb606b1ee478
kapillohana <kapillohana79@gmail.com>
2024-05-13 21:35:21 +0500

    Done Question-14 off 45

---
+++ Question-14/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+// Make a list that includes at least three people you’d like to invite to dinner. 
+// Then use your list to print a message to each person, inviting them to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+let guestList = ["Danesh", "Faizan", "Love", "Amrat"];
+guestList.forEach((guest) => {
+    console.log(`Dear ${guest},\nYou are cordially invited to dinner. Please join us for an evening of great food and stimulating conversation.\n\nBest regards,\n kapil Lohana`);
+});
---
+++ Question-14/index.ts
@@ -0,0 +1,10 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+// Make a list that includes at least three people you’d like to invite to dinner. 
+// Then use your list to print a message to each person, inviting them to dinner.
+    
+
+let guestList: string[] = ["Danesh", "Faizan", "Love", "Amrat"];
+
+guestList.forEach((guest: string) => {
+    console.log(`Dear ${guest},\nYou are cordially invited to dinner. Please join us for an evening of great food and stimulating conversation.\n\nBest regards,\n kapil Lohana`);
+});
