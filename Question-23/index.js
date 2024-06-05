334625a4fece6a6dc827416d2cb457189810c4be
kapillohana <kapillohana79@gmail.com>
2024-06-05 18:21:20 +0500

    23 of 45

---
+++ Question-23/index.js
@@ -0,0 +1,29 @@
+"use strict";
+let car = 'subaru';
+console.log("Is car == 'subaru'? True.");
+console.log(car == 'subaru'); // True
+console.log("Is car == 'toyota'? False.");
+console.log(car == 'toyota'); // False
+let age = 25;
+console.log("Is age > 20?  True.");
+console.log(age > 20); // True
+console.log("Is age < 20?  False.");
+console.log(age < 20); // False
+let color = 'blue';
+console.log("Is color == 'blue'? True.");
+console.log(color == 'blue'); // True
+console.log("Is color == 'red'?  False.");
+console.log(color == 'red'); // False
+let number = 10;
+console.log("Is number >= 10? True.");
+console.log(number >= 10); // True
+console.log("Is number <= 5? False.");
+console.log(number <= 5); // False
+let isStudent = true;
+console.log("Is isStudent == true?  True.");
+console.log(isStudent == true); // True
+let fruits = ['apple', 'banana', 'orange'];
+console.log("Is fruits.includes('banana')?  True.");
+console.log(fruits.includes('banana')); // True
+console.log("Is fruits.includes('grape')?  False.");
+console.log(fruits.includes('grape')); // False
