029eefcc4e2c288d37a6cc1b0586e33e41e483be
kapillohana <kapillohana79@gmail.com>
2024-06-05 22:33:24 +0500

    29 of 45

---
+++ Question-29/index.js
@@ -0,0 +1,33 @@
+"use strict";
+/* Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent
+if statements that check for certain fruits in your array.
+• Make a array of your three favorite fruits and call it favorite_fruits.
+
+• Write five if statements. Each should check whether a certain kind of fruit is in your array.
+If the fruit is in your array, the if block should print a statement, such as You really like bananas!*/
+Object.defineProperty(exports, "__esModule", { value: true });
+let favorite_fruits = ["Banana", "Mango", "Apple"];
+//checks for banana
+if (favorite_fruits.includes("Banana")) {
+    console.log("You really like Bananas?");
+}
+;
+//checks for mango
+if (favorite_fruits.includes("Mango")) {
+    console.log("You really like Mango?");
+}
+;
+//checks for apple
+if (favorite_fruits.includes("Apple")) {
+    console.log("You really like Apple?");
+}
+;
+//checks for kivi
+if (favorite_fruits.includes("kivi")) {
+    console.log("You really like Kivi?");
+}
+;
+//checks for pineapple
+if (favorite_fruits.includes("pineapple")) {
+    console.log("You really like pineapple?");
+}
