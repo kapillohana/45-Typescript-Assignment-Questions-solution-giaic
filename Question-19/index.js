a1aa848f83247e05fd5de5cd6ce0619cbffc026b
kapillohana <kapillohana79@gmail.com>
2024-06-05 16:22:33 +0500

    19 of 45

---
+++ Question-19/index.js
@@ -0,0 +1,45 @@
+"use strict";
+/*Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+print a message indicating the number of people you are inviting to dinner. */
+Object.defineProperty(exports, "__esModule", { value: true });
+// From Question-14
+let guestList = ["Danesh", "Faizan", "Love", "Amrat"];
+guestList.forEach((guest) => {
+    console.log(`Dear ${guest},\nYou are cordially invited to dinner. Please join us for an evening of great food and stimulating conversation.\n\nBest regards,\n kapil Lohana`);
+});
+//From Question-15
+//raheel is not coming to the party so instead of raheel we are inviting danesh my friend to the party
+console.log("unfortunatly Raheel is not avaiable so he couldn't join us.");
+const index = guestList.indexOf("raheel");
+if (index !== -1) {
+    // Remove one item at the found index and insert "Danesh"
+    guestList.splice(index, 1, "Danesh");
+}
+;
+// console.log(guestList)
+//invitation to the new list guests same invitation message
+guestList.forEach((guest) => {
+    console.log(`Dear ${guest},\nYou are cordially invited to dinner. Please join us for an evening of great food and stimulating conversation.\n\nBest regards,\n kapil Lohana`);
+});
+console.log("unfortunatly Raheel is not avaiable so he couldn't join us.");
+//From Question-16
+console.log("\nThere is a good news! we just found bigger dinner table so we have three more friends to join us \n");
+guestList.unshift("Karan");
+guestList.splice(3, 0, "Shazia");
+guestList.push("Bisma");
+guestList.forEach(function (guest) {
+    console.log("Dear ".concat(guest, ",\nYou are cordially invited to dinner. Please join us for an evening of great food and stimulating conversation.\n\nBest regards,\n kapil Lohana"));
+});
+//From Question-17
+console.log("\nUnfortunately, The dinner table could not reach on time so we only have space for two guests\n");
+console.log(guestList);
+// guestList.pop()
+while (guestList.length > 2) {
+    let remove_guest = guestList.pop();
+    console.log(`Sorry ${remove_guest}, You are not invited for Dinner.\n`);
+}
+//inviting two remaining guests
+guestList.forEach((guest) => {
+    console.log(`Dear ${guest},\nYou are cordially invited to dinner. Please join us for an evening of great food and stimulating conversation.\n\nBest regards,\n kapil Lohana`);
+});
+console.log(`\nTotal number of guestes we are inviting are: ${guestList.length}`);
