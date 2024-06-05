f7b11648fd4bdb5a99b77619913a234937f51f79
kapillohana <kapillohana79@gmail.com>
2024-06-05 21:54:23 +0500

    27 of 45

---
+++ Question-27/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+• If the alien is green, print a message that the player earned 5 points.
+
+• If the alien is yellow, print a message that the player earned 10 points.
+
+• If the alien is red, print a message that the player earned 15 points.
+
+• Write three versions of this program, making sure each message is printed for the appropriate color alien. */
+Object.defineProperty(exports, "__esModule", { value: true });
+//condition1 = green alien
+let alienColor1 = "green";
+if (alienColor1 === "green") {
+    console.log("Congratulation! You just earned 5 points ");
+}
+else if (alienColor1 === "yellow") {
+    console.log("Congratulation! You just earned 10 points");
+}
+else if (alienColor1 === "red") {
+    console.log("Congratulation! You just earned 15 points");
+}
+;
+//condition2 = yellow alien
+let alienColor2 = "yellow";
+if (alienColor2 === "green") {
+    console.log("Congratulation! You just earned 5 points ");
+}
+else if (alienColor2 === "yellow") {
+    console.log("Congratulation! You just earned 10 points");
+}
+else if (alienColor2 === "red") {
+    console.log("Congratulation! You just earned 15 points");
+}
+;
+//condition3 = red alien 
+let alienColor3 = "red";
+if (alienColor3 === "green") {
+    console.log("Congratulation! You just earned 5 points ");
+}
+else if (alienColor3 === "yellow") {
+    console.log("Congratulation! You just earned 10 points");
+}
+else if (alienColor3 === "red") {
+    console.log("Congratulation! You just earned 15 points");
+}
+;
---
+++ Question-27/index.ts
@@ -0,0 +1,40 @@
+/*Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+• If the alien is green, print a message that the player earned 5 points.
+
+• If the alien is yellow, print a message that the player earned 10 points.
+
+• If the alien is red, print a message that the player earned 15 points.
+
+• Write three versions of this program, making sure each message is printed for the appropriate color alien. */
+
+//condition1 = green alien
+let alienColor1: string= "green"
+if (alienColor1 === "green"){
+    console.log ("Congratulation! You just earned 5 points ")
+} else if (alienColor1 === "yellow"){
+    console.log ("Congratulation! You just earned 10 points")
+} else if (alienColor1 === "red"){
+    console.log ("Congratulation! You just earned 15 points")
+};
+
+//condition2 = yellow alien
+let alienColor2: string= "yellow"
+if (alienColor2 === "green"){
+    console.log ("Congratulation! You just earned 5 points ")
+} else if (alienColor2 === "yellow"){
+    console.log ("Congratulation! You just earned 10 points")
+} else if (alienColor2=== "red"){
+    console.log ("Congratulation! You just earned 15 points")
+};
+
+//condition3 = red alien 
+let alienColor3: string= "red"
+if (alienColor3 === "green"){
+    console.log ("Congratulation! You just earned 5 points ")
+} else if (alienColor3 === "yellow"){
+    console.log ("Congratulation! You just earned 10 points")
+} else if (alienColor3 === "red"){
+    console.log ("Congratulation! You just earned 15 points")
+};
+
+
