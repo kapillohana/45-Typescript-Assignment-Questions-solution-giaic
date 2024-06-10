aeda9d38b49c36b0a2c5799992cdbafe3edc87a6
kapillohana <kapillohana79@gmail.com>
2024-06-10 17:37:11 +0500

    39 of 45

--- Question-39/index.js
+++ Question-39/index.js
@@ -1,9 +1,13 @@
 "use strict";
 /* City Names: Write a function called city_country() that takes in the name of a city and its country.
 The function should return a string formatted like this:
-"Lahore, Pakistan" */
+"Lahore, Pakistan"
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
 function city_country(city, country) {
     console.log(`"${city}, ${country}"`);
 }
-city_country("hyderabad", "pakistan");
+;
+city_country("Hyderabad", "Pakistan");
+city_country("Newyork", "USA");
+city_country("Berlin", "Germany");
--- Question-39/index.ts
+++ Question-39/index.ts
@@ -1,8 +1,12 @@
 /* City Names: Write a function called city_country() that takes in the name of a city and its country. 
 The function should return a string formatted like this:
-"Lahore, Pakistan" */
+"Lahore, Pakistan" 
+*/
+
 
 function city_country(city: string , country: string ): void{
     console.log (`"${city}, ${country}"`)
 };
 city_country("Hyderabad", "Pakistan");
+city_country("Newyork", "USA");
+city_country("Berlin", "Germany");
