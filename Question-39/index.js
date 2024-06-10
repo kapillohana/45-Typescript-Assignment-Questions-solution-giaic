4f0f9729f59fa42e84ecb9b88a2c5de99cbf2f83
kapillohana <kapillohana79@gmail.com>
2024-06-10 17:30:02 +0500

    39 of 45

---
+++ Question-39/index.js
@@ -0,0 +1,9 @@
+"use strict";
+/* City Names: Write a function called city_country() that takes in the name of a city and its country.
+The function should return a string formatted like this:
+"Lahore, Pakistan" */
+Object.defineProperty(exports, "__esModule", { value: true });
+function city_country(city, country) {
+    console.log(`"${city}, ${country}"`);
+}
+city_country("hyderabad", "pakistan");
